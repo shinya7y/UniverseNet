168cb4cb6c2179a28416a404a793227e8e100718
Yosuke Shinya <42844407+shinya7y@users.noreply.github.com>
2020-05-20 05:38:40 +0000

    Add configs for ATSS with SEPC

---
+++ configs/waymo_open/atss_r50_fpn_sepc_noibn_fp16_1x_waymo_open_f0.py
@@ -0,0 +1,77 @@
+_base_ = [
+    '../_base_/datasets/waymo_open_2d_detection_f0.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+model = dict(
+    type='ATSS',
+    pretrained='torchvision://resnet50',
+    backbone=dict(
+        type='ResNet',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
+        style='pytorch'),
+    neck=[
+        dict(
+            type='FPN',
+            in_channels=[256, 512, 1024, 2048],
+            out_channels=256,
+            start_level=1,
+            add_extra_convs=True,
+            extra_convs_on_inputs=False,
+            num_outs=5),
+        dict(
+            type='SEPC',
+            out_channels=256,
+            stacked_convs=4,
+            pconv_deform=True,
+            lcconv_deform=True,
+            ibn=False,  # please set imgs/gpu >= 4
+        )
+    ],
+    bbox_head=dict(
+        type='ATSSSEPCHead',
+        num_classes=3,
+        in_channels=256,
+        stacked_convs=0,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            ratios=[1.0],
+            octave_base_scale=8,
+            scales_per_octave=1,
+            strides=[8, 16, 32, 64, 128]),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[.0, .0, .0, .0],
+            target_stds=[0.1, 0.1, 0.2, 0.2]),
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='GIoULoss', loss_weight=2.0),
+        loss_centerness=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0)))
+# training and testing settings
+train_cfg = dict(
+    assigner=dict(type='ATSSAssigner', topk=9),
+    allowed_border=-1,
+    pos_weight=-1,
+    debug=False)
+test_cfg = dict(
+    nms_pre=1000,
+    min_bbox_size=0,
+    score_thr=0.05,
+    nms=dict(type='nms', iou_thr=0.6),
+    max_per_img=100)
+# optimizer
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+
+load_from = '../logs/coco/atss_r50_fpn_sepc_noibn_1x_coco_20200518_200416/epoch_12.pth'  # noqa
+# fp16 settings
+fp16 = dict(loss_scale=512.)
