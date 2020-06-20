cce3e6d208e93baca4ebad155bf8ef0c4d368d7b
Yosuke Shinya <42844407+shinya7y@users.noreply.github.com>
2020-06-20 07:34:00 +0000

    Add configs for NightOwls

---
+++ configs/nightowls/atss_r2_50_fpn_sepc_noibn_dcn_fp16_4x4_lr0001_mstrain_640_1280_from_waymo_7e_nightowls.py
@@ -0,0 +1,14 @@
+_base_ = [
+    '../_base_/models/atss_r2_50_fpn_sepc_noibn_dcn.py',
+    '../_base_/datasets/nightowls_mstrain_640_1280.py',
+    '../_base_/schedules/schedule_7e.py', '../_base_/default_runtime.py'
+]
+model = dict(bbox_head=dict(num_classes=3))
+
+data = dict(samples_per_gpu=4)
+
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
+
+fp16 = dict(loss_scale=512.)
+
+load_from = '../data/checkpoints/atss_r2_50_fpn_sepc_noibn_dcn_fp16_8x2_lr0001_mstrain_640_1280_7e_waymo_open_20200526_080330/epoch_7_for_nightowls.pth'  # noqa
