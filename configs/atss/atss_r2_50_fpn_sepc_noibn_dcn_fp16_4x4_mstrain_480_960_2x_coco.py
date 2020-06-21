7c1a4236091a2ff45b679844732e56849855a9b5
Yosuke Shinya <42844407+shinya7y@users.noreply.github.com>
2020-06-21 15:23:06 +0000

    Clean configs

--- configs/atss/atss_r2_50_fpn_sepc_noibn_dcn_fp16_4x4_mstrain_480_960_2x_coco.py
+++ configs/atss/atss_r2_50_fpn_sepc_noibn_dcn_fp16_4x4_mstrain_480_960_2x_coco.py
@@ -1,26 +1,10 @@
 _base_ = [
     '../_base_/models/atss_r2_50_fpn_sepc_noibn_dcn.py',
-    '../_base_/datasets/coco_detection.py',
+    '../_base_/datasets/coco_detection_mstrain_480_960.py',
     '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
 ]
 
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(
-        type='Resize',
-        img_scale=[(1333, 480), (1333, 960)],
-        multiscale_mode='range',
-        keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
-]
-data = dict(samples_per_gpu=4, train=dict(pipeline=train_pipeline))
+data = dict(samples_per_gpu=4)
 
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(
