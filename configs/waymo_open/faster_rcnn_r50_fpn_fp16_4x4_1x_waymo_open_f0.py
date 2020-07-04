a9665c34738c03a2910c790784ca0dfdc68a02e2
Yosuke Shinya <42844407+shinya7y@users.noreply.github.com>
2020-07-04 11:36:10 +0000

    Change pre-trained model of Faster R-CNN

--- configs/waymo_open/faster_rcnn_r50_fpn_fp16_4x4_1x_waymo_open_f0.py
+++ configs/waymo_open/faster_rcnn_r50_fpn_fp16_4x4_1x_waymo_open_f0.py
@@ -8,6 +8,6 @@ model = dict(roi_head=dict(bbox_head=dict(num_classes=3)))
 # data
 data = dict(samples_per_gpu=4)
 
-load_from = 'https://open-mmlab.s3.ap-northeast-2.amazonaws.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'  # noqa
+load_from = 'https://open-mmlab.s3.ap-northeast-2.amazonaws.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_2x_coco/faster_rcnn_r50_fpn_2x_coco_bbox_mAP-0.384_20200504_210434-a5d8aa15.pth'  # noqa
 # fp16 settings
 fp16 = dict(loss_scale=512.)
