f355c7ec9b6791211a1c29376042d8af5dd13e01
Yosuke Shinya <42844407+shinya7y@users.noreply.github.com>
2020-11-15 07:51:13 +0000

    Update GFL with ATSS-style TTA

---
+++ configs/universenet/universenet101_2008d_fp16_4x4_mstrain_480_960_20e_coco_test_vote.py
@@ -0,0 +1,37 @@
+_base_ = './universenet101_2008d_fp16_4x4_mstrain_480_960_20e_coco.py'
+
+# 5 scales and flip
+tta_flip = True
+tta_scale = [(667, 400), (1000, 600), (1333, 800), (1667, 1000), (2000, 1200)]
+scale_ranges = [(96, 10000), (64, 10000), (0, 10000), (0, 10000), (0, 256)]
+
+# 13 scales and flip (slow)
+# tta_flip = True
+# tta_scale = [(667, 400), (833, 500), (1000, 600), (1067, 640), (1167, 700),
+#              (1333, 800), (1500, 900), (1667, 1000), (1833, 1100),
+#              (2000, 1200), (2167, 1300), (2333, 1400), (3000, 1800)]
+# scale_ranges = [(96, 10000), (96, 10000), (64, 10000), (64, 10000),
+#                 (64, 10000), (0, 10000), (0, 10000), (0, 10000), (0, 256),
+#                 (0, 256), (0, 192), (0, 192), (0, 96)]
+
+test_cfg = dict(fusion_cfg=dict(type='soft_vote', scale_ranges=scale_ranges))
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=tta_scale,
+        flip=tta_flip,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    val=dict(pipeline=test_pipeline), test=dict(pipeline=test_pipeline))
