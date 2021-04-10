5b2f3fb7f5123e3d1d3ff823d0ffd75a3c5ae654
Yosuke Shinya <42844407+shinya7y@users.noreply.github.com>
2021-04-10 15:28:58 +0000

    Fix size mismatch in multiclass_nms

--- mmdet/core/post_processing/bbox_nms.py
+++ mmdet/core/post_processing/bbox_nms.py
@@ -76,10 +76,11 @@ def multiclass_nms(multi_bboxes,
         if torch.onnx.is_in_onnx_export():
             raise RuntimeError('[ONNX Error] Can not record NMS '
                                'as it has not been executed this time')
+        dets = torch.cat([bboxes, scores[:, None]], -1)
         if return_inds:
-            return bboxes, labels, inds
+            return dets, labels, inds
         else:
-            return bboxes, labels
+            return dets, labels
 
     dets, keep = batched_nms(bboxes, scores, labels, nms_cfg)
 
