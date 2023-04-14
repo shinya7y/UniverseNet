3f6147c06115e90bac71bec8a5f91b489a4d1e64
Yosuke Shinya <42844407+shinya7y@users.noreply.github.com>
2023-04-15 01:48:35 +0900

    Add configs for mva2023_sod4bird

---
+++ configs/datasets/mva2023_sod4bird/gfl_r50_fpn_fp16_1x2x8_7e_mva2023_sod4bird_2160.py
@@ -0,0 +1,21 @@
+_base_ = [
+    '../../gfl/_gfl_r50_fpn.py', '_mva2023_sod4bird_2160.py',
+    '../../_base_/schedules/schedule_7e.py', '../../_base_/default_runtime.py'
+]
+
+model = dict(
+    bbox_head=dict(num_classes=1),
+    test_cfg=dict(score_thr=0.001, max_per_img=300))
+
+data = dict(samples_per_gpu=2, val=dict(samples_per_gpu=2))
+
+# 1 GPU * 2 samples_per_gpu * 8 cumulative_iters
+# to simulate 8 GPUs * 2 samples_per_gpu
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(cumulative_iters=8)
+lr_config = dict(warmup_iters=4000)  # 500 * cumulative_iters
+
+fp16 = dict(loss_scale=dict(init_scale=512))
+custom_hooks = []
+
+load_from = 'http://download.openmmlab.com/mmdetection/v2.0/gfl/gfl_r50_fpn_1x_coco/gfl_r50_fpn_1x_coco_20200629_121244-25944287.pth'  # noqa
---
+++ configs/datasets/mva2023_sod4bird/gfl_r50_fpn_fp16_1x2x8_7e_mva2023_sod4bird_2160_test.py
@@ -0,0 +1,22 @@
+_base_ = 'gfl_r50_fpn_fp16_1x2x8_7e_mva2023_sod4bird_2160.py'
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(3840, 2160),
+        flip=True,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+data = dict(
+    val=dict(pipeline=test_pipeline), test=dict(pipeline=test_pipeline))
